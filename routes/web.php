7eac4040694ec7083403666dd92cc05acc2d2e56
Geperson Mamalias <125843365+kuyagep@users.noreply.github.com>
2023-06-16 22:11:12 +0800

    Instantly Integrate AdminLTE 3

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Auth::routes();
 
-require __DIR__.'/auth.php';
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
